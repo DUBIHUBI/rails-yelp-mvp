99bdcb2fe826d42d18968b9fd798c8b0a7677b95
Hubert Yu <hubert12161999@gmail.com>
2022-11-16 12:26:40 +0000

    Add indentation and remove unnecessary review routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
+  # get 'reviews/new'
+  # get 'reviews/create'
+  root to: "restaurants#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
